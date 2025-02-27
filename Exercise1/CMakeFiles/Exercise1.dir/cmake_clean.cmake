c1f4d10a3f68e11ddb81fba4babba03e508a3783
ReasonableFailure <dinhfaye@protonmail.com>
2025-02-27 15:34:02 +0100

    bbbbbbbbbbbbbbb

---
+++ Exercise1/CMakeFiles/Exercise1.dir/cmake_clean.cmake
@@ -0,0 +1,21 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Exercise1.dir/src/Cube.cpp.o"
+  "CMakeFiles/Exercise1.dir/src/Cube.cpp.o.d"
+  "CMakeFiles/Exercise1.dir/src/GLApp.cpp.o"
+  "CMakeFiles/Exercise1.dir/src/GLApp.cpp.o.d"
+  "CMakeFiles/Exercise1.dir/src/GLExample.cpp.o"
+  "CMakeFiles/Exercise1.dir/src/GLExample.cpp.o.d"
+  "CMakeFiles/Exercise1.dir/src/Shape.cpp.o"
+  "CMakeFiles/Exercise1.dir/src/Shape.cpp.o.d"
+  "CMakeFiles/Exercise1.dir/src/Torus.cpp.o"
+  "CMakeFiles/Exercise1.dir/src/Torus.cpp.o.d"
+  "CMakeFiles/Exercise1.dir/src/main.cpp.o"
+  "CMakeFiles/Exercise1.dir/src/main.cpp.o.d"
+  "Exercise1"
+  "Exercise1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Exercise1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
